868d5eb9c1c938438b4d1172331efd1e3858a4a9
Maksim Kalnik <kalnikmaxim@gmail.com>
2017-08-31 11:55:56 +0300

    fixes

--- src/js/local.js
+++ src/js/local.js
@@ -1,4 +1,15 @@
 $(document).ready(function () {
     var body = $('body');
+    body.on('click', '.page-header__toggle-btn', function () {
+      event.preventDefault();
+      $('.main-nav').removeClass('main-nav--closed');
+      $('.main-nav').addClass('main-nav--opened');
+    });
+
+    body.on('click', '.page-header__close-btn', function () {
+      event.preventDefault();
+      $('.main-nav').removeClass('main-nav--opened');
+      $('.main-nav').addClass('main-nav--closed');
+    });
 
 });
