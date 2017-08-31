37994b7c9109080d7b3120e889c2de3c087138f2
Maksim Kalnik <kalnikmaxim@gmail.com>
2017-08-31 18:45:53 +0300

    768 added

--- src/js/local.js
+++ src/js/local.js
@@ -4,12 +4,14 @@ $(document).ready(function () {
       event.preventDefault();
       $('.main-nav').removeClass('main-nav--closed');
       $('.main-nav').addClass('main-nav--opened');
+      $('.page-header__top').addClass('page-header__top--opened');
     });
 
     body.on('click', '.page-header__close-btn', function () {
       event.preventDefault();
       $('.main-nav').removeClass('main-nav--opened');
       $('.main-nav').addClass('main-nav--closed');
+      $('.page-header__top').removeClass('page-header__top--opened');
     });
 
 });
